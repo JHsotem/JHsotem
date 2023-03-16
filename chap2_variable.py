0f8a44c70f46a45a7adffd6cd9d5bb7716e16edb
Park Jae Hwan <97014845+JHsotem@users.noreply.github.com>
2023-03-16 13:39:54 +0900

    Add files via upload

---
+++ chap2_variable.py
@@ -0,0 +1,50 @@
+### Chap 2 Variables ###
+'''
+a = 1 + 2
+b = 3 - 4 
+c = -9 / 2 
+d = 9 % 2 
+e = 9 // 2 
+f = -9 // 2 
+g = 9 // -2
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+listA = [a, b, c, d, e, f, g]
+print(listA) 
+print("PJH")
+'''
+'''
+a = 11
+b = 20 
+print(a+b, a-b, a*b, a/b)
+
+'''
+'''
+a = 1 
+print(a)
+print(id(a))
+b = 1 
+print(b)
+print(id(b))
+if id(a) == id(b):
+    print(True)
+else:
+    print(False)
+'''
+'''
+a = input("Plz enter your ID:")
+b = input("Plz enter your password:")
+
+print(a)
+print(b)
+
+'''
+
